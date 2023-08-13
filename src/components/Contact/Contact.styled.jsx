e836819319ad42a4c55313daa567b60ae01864ce
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-13 14:07:24 +0300

    add ui visuality

--- src/components/Contact/Contact.styled.jsx
+++ src/components/Contact/Contact.styled.jsx
@@ -7,12 +7,14 @@ export const DeleteButton = styled.button`
 export const List = styled.ul`
   padding-left: 15px;
   width: 360px;
-  
 `;
 export const ListItem = styled.li`
- margin-bottom: 5px;
+  margin-bottom: 5px;
 `;
 export const P = styled.p`
   margin: 0px;
   font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+  width: 345px;
 `;
