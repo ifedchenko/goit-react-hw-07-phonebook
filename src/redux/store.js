4ae45d70d2714b88577fed7d06f3c166fd3460e7
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-13 14:04:39 +0300

    delete local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,42 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
 
 import { contactSlice, filterSlice } from './slice';
 
-
-const persistContactsConfig = {
-  key: 'contacts',
-  storage,
-};
-
-export const contactReducer = persistReducer(
-  persistContactsConfig,
-  contactSlice.reducer
-);
-
 export const store = configureStore({
   reducer: {
-    contacts: contactReducer,
+    contacts: contactSlice.reducer,
     filter: filterSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
-
