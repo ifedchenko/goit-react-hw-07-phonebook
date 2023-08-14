b50e76ef9fe2e0eea22177521e425933453b5688
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-14 14:33:21 +0300

    Add mount app loader

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts } from '../../redux/operations';
 import { getContacts, getFilter } from '../../redux/selectors';
 import { DeleteButton, List, ListItem, P } from './Contact.styled';
+import Loader from 'components/Loader/Loader';
 
 const Contact = () => {
+  const [isLoaderShown, setIsLoaderShown] = useState(false);
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    setIsLoaderShown(true);
+    dispatch(fetchContacts())
+      .then(() => {
+        setIsLoaderShown(false);
+      })
+      .catch(error => {
+        console.error('Error fetching contacts:', error);
+        setIsLoaderShown(false);
+      });
   }, [dispatch]);
 
   const handleDeleteContact = id => {
@@ -19,25 +29,29 @@ const Contact = () => {
 
   return (
     <>
-      <List>
-        {contacts
-          .filter(contact =>
-            contact.name.toLowerCase().includes(filter.toLowerCase())
-          )
-          .map(({ id, name, phone }) => (
-            <ListItem key={id}>
-              <P>
-                {name}: {phone}
-                <DeleteButton
-                  type="button"
-                  onClick={() => handleDeleteContact(id)}
-                >
-                  Delete
-                </DeleteButton>
-              </P>
-            </ListItem>
-          ))}
-      </List>
+      {isLoaderShown ? (
+        <Loader /> // Render the loader if isLoaderShown is true
+      ) : (
+        <List>
+          {contacts
+            .filter(contact =>
+              contact.name.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map(({ id, name, phone }) => (
+              <ListItem key={id}>
+                <P>
+                  {name}: {phone}
+                  <DeleteButton
+                    type="button"
+                    onClick={() => handleDeleteContact(id)}
+                  >
+                    Delete
+                  </DeleteButton>
+                </P>
+              </ListItem>
+            ))}
+        </List>
+      )}
     </>
   );
 };
